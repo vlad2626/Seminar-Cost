cf672a0b6d05aa3517b81a59959294eeb64c79a7
vlad2626 <57768989+vlad2626@users.noreply.github.com>
2021-02-21 12:45:37 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstInfo = New System.Windows.Forms.ListBox()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstInfo
+        '
+        Me.lstInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lstInfo.FormattingEnabled = True
+        Me.lstInfo.Location = New System.Drawing.Point(145, 41)
+        Me.lstInfo.Name = "lstInfo"
+        Me.lstInfo.Size = New System.Drawing.Size(475, 301)
+        Me.lstInfo.TabIndex = 0
+        '
+        'btnLoad
+        '
+        Me.btnLoad.BackColor = System.Drawing.Color.Gray
+        Me.btnLoad.ForeColor = System.Drawing.Color.MediumBlue
+        Me.btnLoad.Location = New System.Drawing.Point(145, 381)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(116, 34)
+        Me.btnLoad.TabIndex = 1
+        Me.btnLoad.Text = "Load"
+        Me.btnLoad.UseVisualStyleBackColor = False
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.Color.Gray
+        Me.btnSave.ForeColor = System.Drawing.Color.MediumBlue
+        Me.btnSave.Location = New System.Drawing.Point(317, 381)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(116, 34)
+        Me.btnSave.TabIndex = 2
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Gray
+        Me.btnExit.ForeColor = System.Drawing.Color.MediumBlue
+        Me.btnExit.Location = New System.Drawing.Point(504, 381)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(116, 34)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.MintCream
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.lstInfo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstInfo As ListBox
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+
+    End Sub
+
+
+End Class
